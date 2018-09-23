4c11b8f7f69108aeefbbfb172697db5cc6b003e1
Matt Brooks <lessthanepsion@gmail.com>
2018-09-23 03:43:23 -0600

    added separate debug modes, comments

--- fire-strings.ino
+++ fire-strings.ino
@@ -14,8 +14,8 @@ int VIBR_PIN[CHANNELS] = { A0, A1, A2, A3 };
 int SOL_PIN[CHANNELS] = { 10, 11, 8, 9 };
 int CTRL_SENSITIVITY = A6;
 int CTRL_DURATION = A7;
-int PATTERN_PIN = 4;
-int AUTO_PIN = 3;
+int PATTERN_PIN = 7;
+int AUTO_PIN = 6;
 int LED_PIN = 13;
 
 float SENSITIVITY_MAX = 250.0;
@@ -33,23 +33,31 @@ SimpleTimer solenoidTimer;
 int timerId;
 int senseVal;
 int durationVal;
+bool debugBtns = false;
+bool debugSensors = true;
 
 void setup(){
   pinMode(LED_PIN, OUTPUT);
-  Serial.begin(9600); //init serial 9600
+  Serial.begin(9600);
   
-  // clear buffers and set pinmode
+  // clear buffers and set pinmodes for solenoids
   for (int i=0; i<CHANNELS; i++) {
     reset_buffer(i);
     pinMode(SOL_PIN[i], OUTPUT);
   }
 
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
+  // set pinmodes for button input
+  pinMode(PATTERN_PIN, INPUT);
+  pinMode(AUTO_PIN, INPUT);
+  for (int i=2; i<6; i++) {
+    pinMode(i, INPUT);  
+    digitalWrite(i, HIGH);
+  }
   
   timerId = solenoidTimer.setInterval(1000, trigger_solenoids);
 }
 
+// clears the buffer of sensor data
 void reset_buffer(int channel) {
   for (int j=0; j<SAMPLE_SIZE; j++) 
      measurements[channel][j]=0;
@@ -57,20 +65,24 @@ void reset_buffer(int channel) {
   bufferResult[channel] = 0;
 }
 
+// actuates all of the solenoids whose state is set to 1
+// refractory period exists to restrict gas flow and limit re-actuation
 void trigger_solenoids() {
     int solCount = 0;
 
     for(int i=0; i<CHANNELS; i++) {
       if (state[i]==1) {
-//        Serial.println("Solenoid triggered..");
-//        Serial.println(i);
+        //Serial.println("Solenoid triggered..");
+        //Serial.println(i);
         digitalWrite(SOL_PIN[i], HIGH); 
         solCount++;
       }
     }
     
-    Serial.println("Actuation.....");
-    if (solCount > 0) { delay(durationVal); }
+    if (solCount > 0) { 
+      Serial.println("Actuation....."); 
+      delay(durationVal); 
+    }
 
     Serial.println("Resetting buffer....");  
     for(int i=0; i<CHANNELS; i++) {      
@@ -78,27 +90,42 @@ void trigger_solenoids() {
       reset_buffer(i); 
       state[i]=0;           
     }
-
-    Serial.println("Refractory.....");
-    if (solCount > 0) { delay(durationVal * REFRACT_COEFFICIENT); } 
+ 
+    if (solCount > 0) {
+      Serial.println("Refractory.....");
+      delay(durationVal * REFRACT_COEFFICIENT); 
+    } 
 }
 
 void loop(){  
-    int isAuto = digitalRead(AUTO_PIN);
-    int isPattern = digitalRead(PATTERN_PIN);
-    Serial.print(isAuto);
-    Serial.print(",");
-    Serial.println(isPattern);
+    int isAuto = digitalRead(AUTO_PIN); // Auto or manual actuation
+    int isPattern = digitalRead(PATTERN_PIN); // If auto actuation, pattern or sensor
 
-    
     senseVal = SENSITIVITY_MAX * analogRead(CTRL_SENSITIVITY) / 1023;
     durationVal = DURATION_MIN + ((DURATION_MAX - DURATION_MIN) * analogRead(CTRL_DURATION)) / 1023;
-    //Serial.print(senseVal);
-    //Serial.print(",");
-    //Serial.println(durationVal);
+
+    if (debugBtns) {
+      Serial.print(digitalRead(2)); // Valve 1
+      Serial.print(",");
+      Serial.print(digitalRead(3)); // Valve 2
+      Serial.print(",");
+      Serial.print(digitalRead(4)); // Valve 3
+      Serial.print(",");
+      Serial.print(digitalRead(5)); // Valve 4
+      Serial.print(",");
+      Serial.print(isAuto); 
+      Serial.print(",");
+      Serial.print(isPattern);
+      Serial.print(",");
+      Serial.print(durationVal);
+      Serial.print(",");
+      Serial.println(senseVal);
+    }
+
     
     if (readAnalogVibration) {
-      //TODO: make the timer invoke a callback function starting from when the first sensor passes threshold value
+      //TODO: make the timer invoke a callback function starting 
+      //from when the first sensor passes threshold value
       solenoidTimer.run();
             
       for (int k=0; k<CHANNELS; k++) {
@@ -115,7 +142,7 @@ void loop(){
       }
 
       // output to serial monitor
-      if (DEBUG) {         
+      if (debugSensors ) {         
         Serial.print(bufferResult[0]);
         Serial.print(",");
         Serial.print(bufferResult[1]);
@@ -128,7 +155,7 @@ void loop(){
 }
 
 
-// implement circular buffer for moving average
+// implements circular buffer for moving average
 void piezo_moving_average(int channel, float measurement) {
   int curPos = iter[channel];                
   float newVal = measurement / (float)SAMPLE_SIZE;  
